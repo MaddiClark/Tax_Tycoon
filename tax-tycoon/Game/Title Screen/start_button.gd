68c69d1c4c4a6fe17ed12efffb7e0448eb7d5a5e
Maddi Clark <cclar@ku.edu>
2026-04-18 20:31:29 -0500

    button go brrrr

--- tax-tycoon/Game/Title Screen/start_button.gd
+++ tax-tycoon/Game/Title Screen/start_button.gd
@@ -1,10 +1,14 @@
-extends Node2D
+extends TextureButton
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_tree().change_scene_to_file("res://title.tscn")
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_pressed() -> void:
+	get_tree().change_scene_to_file("res://Game/Farm Screen/farm.tscn")
