4649a5eca5c13d81f83343db151ccc66c9bb9362
Maddi Clark <cclar@ku.edu>
2026-04-18 19:52:17 -0500

    Added start game button to title screen

--- tax-tycoon/title.gd
+++ tax-tycoon/title.gd
@@ -1,17 +1,17 @@
 extends Node2D
 
 
-@onready var startGame = $"World/Title/Start Game"
+@onready var start_button = $"Start Button"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-	startGame.button_pressed.connect(_start_pressed)
+	start_button.button_pressed.connect(_start_game)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 
-func _start_pressed() -> void:
-	pass
+func _start_game() -> void:
+	get_tree().change_scene("res://farm.tscn")
