f88125ce2364dd0fce4743ddc2b42ed54235fc83
Maddi Clark <cclar@ku.edu>
2026-04-18 21:41:44 -0500

    still trying to make count global

--- tax-tycoon/global.gd
+++ tax-tycoon/global.gd
@@ -1,10 +1,11 @@
-extends Node2D
+extends Node
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_tree().call_deferred("change_scene_to_file", "res://Game/Title Screen/title.tscn")
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	pass
