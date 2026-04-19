d71db65f17870d1ddfe3ae9612453ffbe0bddabc
Maddi Clark <cclar@ku.edu>
2026-04-18 20:19:45 -0500

    messing with buttons changing scenes

--- tax-tycoon/upgrade_button.gd
+++ tax-tycoon/upgrade_button.gd
@@ -1,9 +1,10 @@
-extends Node2D
+extends TextureButton
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_tree().change_scene_to_file("res://title.tscn")
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
